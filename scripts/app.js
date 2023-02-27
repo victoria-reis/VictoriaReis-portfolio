749f09d69009d173ca5daf1301eda0eb29a23e2c
Victoria <victoriareis_@outlook.com>
2023-02-27 17:00:20 -0500

    deleted comments and unused files

--- scripts/app.js
+++ scripts/app.js
@@ -30,18 +30,6 @@ const init = () => {
 		loop: true,
 	});
 
-	// portrait switch on mouseover
-	// const portrait = document.querySelector("img");
-	// portrait.addEventListener("mouseenter", () => {
-	// 	portrait.src = "./assets/images/portrait-2-2.jpg";
-	// 	portrait.alt = "portrait of Victoria Reis laughing";
-	// });
-
-	// portrait.addEventListener("mouseleave", () => {
-	// 	portrait.src = "./assets/images/portrait-1.jpg";
-	// 	portrait.alt = "portrait of Victoria Reis";
-	// });
-
 	// mobile menu button open/close
 	const mobileMenuIcon = document.querySelector(".nav-toggle");
 	const navList = document.querySelector(".nav-list");
@@ -50,8 +38,6 @@ const init = () => {
 		if (mobileMenuIcon.classList[1] === "opened") {
 			navList.classList.add("open-menu");
 		} else {
-			// document.querySelector(".navigation .wrapper").style.right = "-100vw";
-			// mobileMenuIcon.style.backgroundColor = "rgba(189, 94, 194, 0.7)";
 			navList.classList.remove("open-menu");
 		}
 	});
@@ -63,23 +49,6 @@ const init = () => {
 			navList.classList.remove("open-menu");
 		});
 	}
-
-	// const formButton = document.querySelector("#form-button");
-	// const form = document.getElementsByTagName("form");
-	// const contactContent = document.querySelector(".contact-content");
-
-	// formButton.addEventListener("submit", (event) => {
-	// 	event.preventDefault();
-	// 	const p = document.createElement("p");
-	// 	p.innerText = "Thank you for reaching out!";
-	// 	form.style.display = "none";
-	// 	contactContent.appendChild(p);
-	// });
-
-	// navList.children.addEventListener("click", () => {
-	// 	mobileMenuIcon.classList.remove("opened");
-	// 	navList.classList.remove("open-menu");
-	// });
 };
 
 init();
